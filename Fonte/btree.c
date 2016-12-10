6bf116457560edb32a34f7e6fd5c05f14da94b86
Jeffersoncoppini <jeffecoppini1@gmail.com>
2016-12-10 20:04:49 -0200

    alterações b+

--- Fonte/btree.c
+++ Fonte/btree.c
@@ -3,9 +3,10 @@
 **/
 typedef struct nodo{
 	char **data
-	char *indice;
+	char **indice;
 	struct nodo *pai, *prox, *ant;
 	struct nodo **filhos;
+	int quant_data;
 } nodo;
 
 //quando o usuário cria uma tabela, está função deve ser chamada para criar o arquivo de indice.
@@ -18,3 +19,11 @@ int inicializa_indice(char* nomeTabela){
 	return 1;	
 }
 
+insere_indice(char* inidice, char* nomeTabela, char* endereco){
+	nodo *aux;
+	aux = constroi_b+(nomeTabela);// retorna raiz
+	aux = busca_insere(aux,inidice,endereco); // retorna inicio da lista dos folhas
+	if(aux == NULL) aux = criaNodo(indice,endereco);
+	insere_arquivo(aux);
+	destroi_arvore(aux);
+}
